3a40c090ea47786c1390371dbef5e09cc7da495a
Adrian Eduardo Gonzalez Barreda <81177394+Adrianbarreda@users.noreply.github.com>
2022-05-27 16:53:16 -0400

    Adding database access

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -1,22 +1,19 @@
 package model.services;
 
-import java.util.ArrayList;
+/*import java.util.ArrayList;*/
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
 
-	/*private DepartmentDao dao = DaoFactory.createDepartmentDao();*/		
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();		
 	
 	public List<Department> findAll() { /* Vamos mockar os dados: MOCK só quando
-	 retorna os dados de "mentirinha" 
-		return dao.findAll();*/
-		List<Department> list = new ArrayList<>();
-		list.add(new Department(1, "Books"));
-		list.add(new Department(2, "Computers"));
-		list.add(new Department(3, "Eletronics"));
-		return list;
+	 retorna os dados de "mentirinha"*/
+		return dao.findAll();
 	}
 
 	/*public void saveOrUpdate(Department obj) {
